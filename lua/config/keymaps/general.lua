a5fc8c14b5f3f1cc3c14dba79c3c9f1b21e472d4
LeoBarreiro <a.leo.barreiro@gmail.com>
2023-03-12 20:54:30 -0300

    chore[keymaps]: fix some style and migrate some maps from old config

--- lua/config/keymaps/general.lua
+++ lua/config/keymaps/general.lua
@@ -1,10 +1,17 @@
-local Util = require("core.utils")
+local util_has = require("core.utils").has
 local map = require("core.utils").map
 
-map("n", "<leader><leader>x", Util.save_and_exec)
+map("n", "<leader><leader>x", require("core.utils").save_and_exec, { desc = "Save and execute the current file" })
+
+map('', '<leader>z', '<cmd>call ToggleWrap()<cr>',{ desc = "Toggle line wrap" })
+map('n', '<F7>', 'gg=G<C-o>', { desc = "Reindent file"})
 
 map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
 map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
+
+-- FIXME: Doesn't work with Tmux or ZSH
+-- Delete the previous word.
+map('i', '<C-BS>', '<C-W>', { desc = "Delete the previous word" })
 
 -- Move Lines
 map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
@@ -17,26 +24,28 @@ map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 -- Search
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
 map({ "n", "x" }, "gw", "*N", { desc = "Search word under cursor" })
+map('n', 'n', 'nzz', { desc = "Next search result and center" })
+map('n', 'N', 'Nzz', { desc = "Prev search result and center" })
 
 
-if not Util.has("trouble.nvim") then
-  map("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
-  map("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
+if not util_has("trouble.nvim") then
+    map("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
+    map("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
 end
 
 -- toggle options
-map("n", "<leader>ud", Util.toggle_diagnostics, { desc = "Toggle Diagnostics" })
+map("n", "<leader>ud", require("core.utils").toggle_diagnostics, { desc = "Toggle Diagnostics" })
 
 -- lazygit
-map("n", "<leader>gg", function() Util.float_term({ "lazygit" }, { cwd = Util.get_root() }) end, { desc = "Lazygit (root dir)" })
-map("n", "<leader>gG", function() Util.float_term({ "lazygit" }) end, { desc = "Lazygit (cwd)" })
+map("n", "<leader>gg", function() require("core.utils").float_term({ "lazygit" }, { cwd = require("core.utils").get_root() }) end, { desc = "Lazygit (root dir)" })
+map("n", "<leader>gG", function() require("core.utils").float_term({ "lazygit" }) end, { desc = "Lazygit (cwd)" })
 
 -- highlights under cursor
 if vim.fn.has("nvim-0.9.0") == 1 then
-  map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+    map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
 end
 
 -- floating terminal
-map("n", "<leader>ft", function() Util.float_term(nil, { cwd = Util.get_root() }) end, { desc = "Terminal (root dir)" })
-map("n", "<leader>fT", function() Util.float_term() end, { desc = "Terminal (cwd)" })
+map("n", "<leader>ft", function() require("core.utils").float_term(nil, { cwd = require("core.utils").get_root() }) end, { desc = "Terminal (root dir)" })
+map("n", "<leader>fT", function() require("core.utils").float_term() end, { desc = "Terminal (cwd)" })
 map("t", "<esc><esc>", "<c-\\><c-n>", {desc = "Enter Normal Mode"})
---
+++ readme.md
@@ -0,0 +1,5 @@
+Resources link:
+- Old config:               https://github.com/barreiroleo/nvim-config
+- Tips and tricks:          https://github.com/mhinz/vim-galore
+- Lazy.nvim plugin manager: https://github.com/folke/lazy.nvim
+- LazyVim distro:           https://github.com/LazyVim/LazyVim
