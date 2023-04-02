cd98a6735852d0e8c59df6ed5affbf6f1fb142ac
LeoBarreiro <a.leo.barreiro@gmail.com>
2023-04-01 23:06:39 -0300

    fix(lsp): Wrong lazy event. Should be at BufRead and BufNew events

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,5 @@
 return {
     { "neovim/nvim-lspconfig",
-        -- event = { "BufReadPre", "BufNewFile" },
         dependencies = {
             { "williamboman/mason.nvim" },
             { "williamboman/mason-lspconfig.nvim" },
@@ -16,45 +15,15 @@ return {
             { "SmiteshP/nvim-navic" },
             { "ray-x/lsp_signature.nvim" },
 
-            { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-            { "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
-            { "hrsh7th/cmp-nvim-lsp",
-                cond = function() return require("core.utils").has("nvim-cmp") end
+            { "folke/neoconf.nvim",
+                cmd = "Neoconf",
+                config = true
+            },
+            { "folke/neodev.nvim",
+                opts = { experimental = { pathStrict = true } }
             },
         },
-        event = "VeryLazy",
+        event = { "BufReadPre", "BufNewFile" },
         config = function() require "core.lsp" end,
     },
-    { 'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        dependencies = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
-        },
-        config = function()
-            -- local lsp = require('lsp-zero').preset({
-            --     name = 'minimal',
-            --     set_lsp_keymaps = true,
-            --     manage_nvim_cmp = true,
-            --     suggest_lsp_servers = false,
-            -- })
-            -- -- (Optional) Configure lua language server for neovim
-            -- lsp.nvim_workspace()
-            -- lsp.setup()
-        end
-    }
 }
