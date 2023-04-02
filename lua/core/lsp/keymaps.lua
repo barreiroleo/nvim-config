824eeb66e20446c77e68464eac0c1056a012539d
LeoBarreiro <a.leo.barreiro@gmail.com>
2023-04-02 14:32:53 -0300

    fix(lsp): Go to definition, declaration, implemantation

--- lua/core/lsp/keymaps.lua
+++ lua/core/lsp/keymaps.lua
@@ -5,7 +5,7 @@ local on_attach = function(_, bufnr)
     map('n', 'gD',         ":lua vim.lsp.buf.declaration()<CR>",       {buffer = bufnr})
     map('n', 'gi',         ":lua vim.lsp.buf.implementation()<CR>",    {buffer = bufnr})
     map('n', 'gr',         ":lua vim.lsp.buf.references()<CR>",        {buffer = bufnr})
-    map('n', 'gt',  ":lua vim.lsp.buf.type_definition()<CR>",   {buffer = bufnr})
+    map('n', 'gt',         ":lua vim.lsp.buf.type_definition()<CR>",   {buffer = bufnr})
     map('n', 'K',          ":lua vim.lsp.buf.hover()<CR>",             {buffer = bufnr})
     map('n', '<C-k>',      ":lua vim.lsp.buf.signature_help()<CR>",    {buffer = bufnr})
     map('n', '<leader>rn', ":lua vim.lsp.buf.rename()<CR>",            {buffer = bufnr})
@@ -17,6 +17,10 @@ local on_attach = function(_, bufnr)
     map('n', 'gpd', require("core.lsp.utils.peek").PeekDefinition, { buffer = bufnr })
     map('n', 'gpD', require("core.lsp.utils.peek").PeekDeclaration, { buffer = bufnr })
     map('n', 'gpi', require("core.lsp.utils.peek").PeekImplementation, { buffer = bufnr })
+
+    map("n", "gsd", ":vsplit<CR>gd" , {buffer = bufnr})
+    map("n", "gsD", ":vsplit<CR>gD" , {buffer = bufnr})
+    map("n", "gsi", ":vsplit<CR>gi" , {buffer = bufnr})
 end
 
 return on_attach
