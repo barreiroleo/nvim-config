596c061c3040bf4b12eee16dd4157ae97b42ef0a
LeoBarreiro <a.leo.barreiro@gmail.com>
2023-04-03 21:39:27 -0300

    feat(util): Less command. Drop vim command output to a temporary buffer

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -38,3 +38,15 @@ end
 -- for _, char in ipairs(chars) do
 --     DROP_TEX(char .. filename .. char, 1)
 -- end
+
+-- Command less. Usefull to inspect vim outputs in teporary buffer
+-- Use: <cmd>less messages<cr>
+vim.cmd[[
+    command! -nargs=+ -complete=command Less call Less(win_getid(), <q-mods>, <q-args>)
+
+    function! Less(winid, mods, args) abort
+        execute (len(a:mods) ? a:mods . ' new' : 'botr 20new')
+        call setline(1,split(win_execute(a:winid, a:args),"\n"))
+        setl bt=nofile bh=wipe nobl nomod
+    endfunction
+]]
