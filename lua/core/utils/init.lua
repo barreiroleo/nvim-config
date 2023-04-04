f889ae3688718cbe5c59df7c5f8b539507a464a1
LeoBarreiro <a.leo.barreiro@gmail.com>
2023-04-03 21:38:52 -0300

    feat(lsp): Format function. Use null-ls if available

--- lua/core/utils/init.lua
+++ lua/core/utils/init.lua
@@ -34,6 +34,17 @@ function M.has(plugin)
     return require("lazy.core.config").plugins[plugin] ~= nil
 end
 
+-- Return the loaded options for a plugin
+---@param name string
+function M.get_opts(name)
+    local plugin = require("lazy.core.config").plugins[name]
+    if not plugin then
+        return {}
+    end
+    local Plugin = require("lazy.core.plugin")
+    return Plugin.values(plugin, "opts", false)
+end
+
 -- TODO: Migrate to Lua and proper keymap
 require("core.utils.search_replace_v")
 require("core.utils.HighlightRepeats")
