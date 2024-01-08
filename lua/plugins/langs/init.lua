b10958e66991d41a23d34c20798bd43a5a1259e8
LeoBarreiro <a.leo.barreiro@gmail.com>
2024-01-08 02:02:44 -0300

    fix: Plantuml previewer

--- lua/plugins/langs/init.lua
+++ lua/plugins/langs/init.lua
@@ -25,20 +25,24 @@ return {
     -- Latex
     { import = "plugins.langs.latex" },
     -- PlantUML
-    { 'scrooloose/vim-slumlord', lazy = true,
-        ft = { 'puml', 'uml', 'md' }
-    },
     { 'weirongxu/plantuml-previewer.vim', lazy = true,
-        ft = { 'puml', 'uml', 'md' },
+        ft = { 'plantuml', 'puml', 'uml', 'md' },
         dependencies = {
             'tyru/open-browser.vim',
             'aklt/plantuml-syntax',
         },
+        init = function()
+            -- Custom filetypes
+            vim.filetype.add({
+                extension = {
+                    puml = function(path, bufnr) return 'plantuml' end
+                }
+            })
+        end,
         config = function()
-            vim.cmd [[
-            let g:plantuml_previewer#plantuml_jar_path = '~/.local/bin/plantuml.jar'
-            let g:plantuml_previewer#save_format = 'svg'
-            ]]
+            vim.g.plantuml_previewer = { plantuml_jar_path = vim.fs.normalize('~/.local/bin/plantuml.jar') }
+            vim.g.plantuml_previewer = { save_format = 'svg' }
+            vim.g.plantuml_previewer = { debug_mode = 1 }
         end,
     },
 }
