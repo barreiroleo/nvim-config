71b0bf3be5af36115c37d8b9a823514bc15e4e11
LeoBarreiro <a.leo.barreiro@gmail.com>
2024-01-28 16:54:01 -0300

    ref: cleanup clangd flags and keymaps

--- lua/config/keymaps/general.lua
+++ lua/config/keymaps/general.lua
@@ -1,58 +1,46 @@
 local util_has = require("core.utils").has
-local map = require("core.utils").map
 
-map('n', 'gqf', require("core.lsp.utils.format"), { desc = "LSP: Format the whole file, use null-ls if available" })
-map("n", "<leader><leader>x", require("core.utils").save_and_exec, { desc = "Save and execute the current file" })
-map("n", "<leader>m", "<cmd>messages<cr>", { desc = "Open vim messages" })
+vim.keymap.set('n', 'gqf', require("core.lsp.utils.format"),                  { desc = "LSP: Format the whole file, use null-ls if available" })
+vim.keymap.set("n", "<leader><leader>x", require("core.utils").save_and_exec, { desc = "Save and execute the current file" })
+vim.keymap.set("n", "<leader><leader>c", ":<up>",                             { desc = "Run previous command" })
+vim.keymap.set("n", "<leader>m", "<cmd>messages<cr>",                         { desc = "Open vim messages" })
 
-map('', '<leader>z', '<cmd>call ToggleWrap()<cr>',{ desc = "Toggle line wrap" })
-map('n', '<F7>', 'gg=G<C-o>', { desc = "Reindent file"})
+vim.keymap.set('', '<leader>z', '<cmd>call ToggleWrap()<cr>', { desc = "Toggle line wrap" })
 
-map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
-map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
-
--- FIXME: Doesn't work with Tmux or ZSH
--- Delete the previous word.
-map('i', '<C-BS>', '<C-W>', { desc = "Delete the previous word" })
+vim.keymap.set("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
+vim.keymap.set("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
 
 -- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==",        { desc = "Move down" })
+vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==",        { desc = "Move up" })
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv",        { desc = "Move down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv",        { desc = "Move up" })
 
 -- Search
-map({ "n", "v" }, "/", "/\\v", { desc = "Search with regex by default" })
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
-map({ "n", "x" }, "fw", "*N", { desc = "Search word under cursor" })
-map('n', 'n', 'nzz', { desc = "Next search result and center" })
-map('n', 'N', 'Nzz', { desc = "Prev search result and center" })
+vim.keymap.set({ "n", "v" }, "/", "/\\v",                  { desc = "Search with regex by default" })
+vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+vim.keymap.set({ "n", "x" }, "fw", "*N",                   { desc = "Search word under cursor" })
+vim.keymap.set('n', 'n', 'nzz',                            { desc = "Next search result and center" })
+vim.keymap.set('n', 'N', 'Nzz',                            { desc = "Prev search result and center" })
 
 
 if not util_has("trouble.nvim") then
-    map("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
-    map("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
+    vim.keymap.set("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
+    vim.keymap.set("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
 end
 
--- lazygit
-map("n", "<leader>gg", function() require("core.utils").float_term({ "lazygit" }, { cwd = require("core.utils").get_root() }) end, { desc = "Lazygit (root dir)" })
-map("n", "<leader>gG", function() require("core.utils").float_term({ "lazygit" }) end, { desc = "Lazygit (cwd)" })
-
--- highlights under cursor
-if vim.fn.has("nvim-0.9.0") == 1 then
-    map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
-end
+vim.keymap.set("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
 
 -- floating terminal
-map("n", "<leader>ft", function() require("core.utils").float_term(nil, { cwd = require("core.utils").get_root() }) end, { desc = "Terminal (root dir)" })
-map("n", "<leader>fT", function() require("core.utils").float_term() end, { desc = "Terminal (cwd)" })
-map("t", "<esc><esc>", "<c-\\><c-n>", {desc = "Enter Normal Mode"})
+vim.keymap.set("n", "<leader>ft", function() require("core.utils").float_term(nil,   { cwd = require("core.utils").get_root() }) end, { desc = "Terminal (root dir)" })
+vim.keymap.set("n", "<leader>fT", function() require("core.utils").float_term() end, { desc = "Terminal (cwd)" })
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>",                                     { desc = "Enter Normal Mode"})
 
 -- Diagnostics
-map("n", "<leader>ud", require("core.utils").toggle_diagnostics, { desc = "Toggle Diagnostics" })
--- map('n', '<leader>e', ":lua vim.diagnostic.open_float()<CR>",    { desc = "Open diagnostics in float" })
-map('n', '[d', ":lua vim.diagnostic.goto_prev()<CR>",            { desc = "Go to previous diagnostics" })
-map('n', ']d', ":lua vim.diagnostic.goto_next()<CR>",            { desc = "Go to next diagnostics" })
-map('n', '<leader>q', ":lua vim.diagnostic.setloclist()<CR>",    { desc = "Location of diagnostics" })
+vim.keymap.set("n", "<leader>ud", require("core.utils").toggle_diagnostics, { desc = "Toggle Diagnostics" })
+-- vim.keymap.set('n', '<leader>e', ":lua vim.diagnostic.open_float()<CR>", { desc = "Open diagnostics in float" })
+vim.keymap.set('n', '[d', ":lua vim.diagnostic.goto_prev()<CR>",            { desc = "Go to previous diagnostics" })
+vim.keymap.set('n', ']d', ":lua vim.diagnostic.goto_next()<CR>",            { desc = "Go to next diagnostics" })
+vim.keymap.set('n', '<leader>q', ":lua vim.diagnostic.setloclist()<CR>",    { desc = "Location of diagnostics" })
--- lua/core/lsp/handlers/clangd_flags.lua
+++ lua/core/lsp/handlers/clangd_flags.lua
@@ -11,37 +11,28 @@ end
 
 local compilation_flags = {
     "--compile-commands-dir='build'",
-    '--compile_args_from=' .. pick_str { "lsp", "filesystem" }          -- TEST: Default filesystem
+    '--compile_args_from=' .. pick_str { "lsp", "filesystem" } -- TEST: Default filesystem
+    -- '--compile_args_from=' .. "filesystem"                  -- TEST: Default filesystem
 }
 
 local feature = {
-    '--all-scopes-completion=false',
-    '--clang-tidy=true',
-    '--completion-parse=auto',                                          -- Run parser if preamble are ready. Otherwise, text-based completion.
-    '--completion-style=bundled',                                       -- Pack overloads for instance
-
+    '--all-scopes-completion=true',
     '--fallback-style=webkit',                                          -- "{BasedOnStyle: LLVM, IndentWidth: 4}" is incompatible with .clang-format files
-    '--hidden-features',                                                -- TEST: Def false - Enable hidden features mostly useful to clangd developers
-    '--import-insertions=true',                                         -- TEST: Def false - If header insertion is enabled, add #import directives when accepting code completions or fixing includes in Objective-C code
     '--header-insertion-decorators=false',                              -- https://github.com/hrsh7th/nvim-cmp/issues/999#issuecomment-1130074680
-    '--include-ineligible-results=true',                                -- TEST: Def false - Include ineligible completion results (e.g. private members)
     '--ranking-model=' .. pick_str { "heuristics", "decision_forest" }, -- TEST: Def decision_forest - Model to use to rank code-completion items
 }
 
 local miscellaneous = {
-    '--j=8',                                                            -- TEST: Unk def   - Number of async workers used by clangd. Background index also uses this many workers.
-    '--pch-storage=' .. pick_str { "disk", "memory" },                  -- TEST: Def disk  - Storing PCHs in memory increases memory usages, but may improve performance
-    '--sync',                                                           -- TEST: Def false - Handle client requests on main thread. Background index still uses its own thread.
-    '--use-dirty-headers=true',                                         -- Use files open in the editor when parsing headers instead of reading from the disk
-    '--offset-encoding=utf-16',                                         -- temporary fix for null-ls
+    '--j=8',                                           -- TEST: Unk def   - Number of async workers used by clangd. Background index also uses this many workers.
+    '--pch-storage=' .. pick_str { "disk", "memory" }, -- TEST: Def disk  - Storing PCHs in memory increases memory usages, but may improve performance
+    '--sync',                                          -- TEST: Def false - Handle client requests on main thread. Background index still uses its own thread.
+    '--use-dirty-headers=true',                        -- Use files open in the editor when parsing headers instead of reading from the disk
+    '--offset-encoding=utf-16',                        -- temporary fix for null-ls
 }
 
 local protocol_and_logging = {
-    '--log=' .. pick_str { "error", "info", "verbose" },                -- TEST: Def info  - Verbosity of log messages written to stderr
+    '--log=verbose', -- TEST: Def info  - Verbosity of log messages written to stderr
 }
-
--- P(vim.list_extend({ "clangd" }, vim.tbl_flatten { compilation_flags, feature, miscellaneous, protocol_and_logging }))
--- "clangd --compile-commands-dir='build' --compile_args_from=filesystem --all-scopes-completion=false --clang-tidy=true --completion-parse=never --completion-style=detailed --debug-origin=true --fallback-style='{Base dOnStyle: LLVM, IndentWidth: 4}' --header-insertion-decorators=false --hidden-features --import-insertions=true --include-ineligible-results=true --ranking-model=heuristics --j=8 --pch-storage=memory --sync --use- dirty-headers=true --offset-encoding=utf-16 --log=info"
 
 return {
     randomized = randomized,
