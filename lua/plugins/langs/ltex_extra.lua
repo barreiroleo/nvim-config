3bb7d2e4bf6b3793a30613c96c902eff065cf345
LeoBarreiro <a.leo.barreiro@gmail.com>
2024-04-24 21:11:40 -0300

    fix: ltex_extra options

--- lua/plugins/langs/ltex_extra.lua
+++ lua/plugins/langs/ltex_extra.lua
@@ -3,7 +3,7 @@ local ltex_filetypes = { 'bib', 'markdown', 'org', 'plaintex', 'rst', 'rnoweb', 
 local ltex_opts = {
     settings = {
         checkFrequency = 'save',
-        language = 'es-AR',
+        language = { "en-US", 'es-AR' },
         additionalRules = {
             enablePickyRules = true,
             motherTongue = 'es-AR',
@@ -17,20 +17,20 @@ return {
     opts = {
         init_check = true,                 -- boolean : whether to load dictionaries on startup
         load_langs = { 'es-AR', 'en-US' }, -- table <string> : language for witch dictionaries will be loaded
-        log_level = "error",               -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
+        log_level = "info",                -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
         path = ".ltex",                    -- string : path to store dictionaries. Relative path uses current working directory
         -- path = vim.fn.expand("~") .. "/.local/share/ltex",
-        server_start = true,               -- boolean : Enable the call to ltex. Usefull for migration and test
-        server_opts = {
-            on_attach = function(client, bufnr)
-                print("Loading ltex from ltex_extra")
-                require("core.lsp.opts").on_attach(client, bufnr)
-            end,
-            capabilities = require("core.lsp.opts").capabilities,
-            filetypes = ltex_filetypes,
-            settings = {
-                ltex = ltex_opts
-            }
-        },
+        server_start = false,              -- boolean : Enable the call to ltex. Usefull for migration and test
+        -- server_opts = {
+        --     on_attach = function(client, bufnr)
+        --         print("Loading ltex from ltex_extra")
+        --         require("core.lsp.opts").on_attach(client, bufnr)
+        --     end,
+        --     capabilities = require("core.lsp.opts").capabilities,
+        --     filetypes = ltex_filetypes,
+        --     settings = {
+        --         ltex = ltex_opts
+        --     }
+        -- },
     },
 }
