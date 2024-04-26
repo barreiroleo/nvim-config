000bd7369587728f2473c56f9260daf3db2bfeb8
LeoBarreiro <a.leo.barreiro@gmail.com>
2024-04-25 23:53:13 -0300

    clean up

--- lua/plugins/ui/blankline.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    event = { 'BufNewFile', 'BufReadPre' },
-    opts = {
-        indent = { char = "┆" },
-    },
-}
--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -1,20 +1,68 @@
+local function OpenOutline()
+    local outline = require("outline")
+
+    if not outline.is_open() then
+        outline.open()
+    else
+        outline.focus_toggle()
+    end
+end
+
 return {
-    { 'folke/trouble.nvim', lazy = true,
+    { import = 'plugins.ui.bufferline' },
+    { 'Bekaboo/dropbar.nvim' },
+    {
+        'folke/trouble.nvim',
+        lazy = true,
         branch = "dev",
-        cmd = { 'Trouble', 'TodoTrouble' },
-        dependencies = 'kyazdani42/nvim-web-devicons',
+        event = { 'CmdlineEnter' }
     },
-    { 'norcalli/nvim-colorizer.lua', lazy = true,
+    {
+        'norcalli/nvim-colorizer.lua',
+        lazy = true,
         event = { 'BufNewFile', 'BufReadPre' },
         opts = { 'md', 'tex', 'json', 'css', 'javascript', 'html', 'python', 'lua' },
-        cmd = {"ColorizerAttachToBuffer"}
+        cmd = { "ColorizerAttachToBuffer" }
     },
-    { "RRethy/vim-illuminate", lazy = true,
+    {
+        "RRethy/vim-illuminate",
+        lazy = true,
         event = "VeryLazy"
     },
-    { 'Bekaboo/dropbar.nvim' },
-    { import = 'plugins.ui.blankline' },
-    { import = 'plugins.ui.bufferline' },
-    { import = 'plugins.ui.lualine' },
-    { import = 'plugins.ui.outline' },
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        main = 'ibl',
+        event = { 'BufNewFile', 'BufReadPre' },
+        opts = {
+            indent = { char = "┆" },
+        },
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        enabled = true,
+        event = "VeryLazy",
+        dependencies = {
+            { "kyazdani42/nvim-web-devicons" }
+        },
+        opts = {
+            options = {
+                component_separators = '|',
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = { 'neo-tree', 'TelescopePrompt', },
+                always_divide_middle = false,
+                globalstatus = false,
+            }
+        }
+    },
+    {
+        'hedyhli/outline.nvim',
+        lazy = true,
+        cmd = 'Outline',
+        keys = { { '<leader>cs', OpenOutline, desc = 'Symbols LSP navigation' } },
+        config = function()
+            require('outline').setup {
+                preview_window = { live = true },
+            }
+        end,
+    }
 }
