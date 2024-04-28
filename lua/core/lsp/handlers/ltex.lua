898ece2478b84c7f270cc64d77d873223bbc56b8
LeoBarreiro <a.leo.barreiro@gmail.com>
2024-04-28 18:43:35 -0300

    refactor: ltex server config and ltex_extra setup

--- lua/core/lsp/handlers/ltex.lua
+++ lua/core/lsp/handlers/ltex.lua
@@ -1,42 +1,21 @@
 local def_opts = require("core.lsp.opts")
-local M = {}
 
-M.filetypes = { 'bib', 'markdown', 'org', 'plaintex', 'rst', 'rnoweb', 'tex' } --gitcommit
-
-M.settings = {
-    settings = {
-        checkFrequency = 'save',
-        language = { "en-US", 'es-AR' },
-        additionalRules = {
-            enablePickyRules = true,
-            motherTongue = 'es-AR',
-        },
-    }
-}
-
-
-M.opts = {
+return {
     autostart = true,
     on_attach = function(client, bufnr)
         vim.notify("Loading ltex from lspconfig")
         def_opts.on_attach(client, bufnr)
-        require('ltex_extra').setup({
-            init_check = true,
-            load_langs = { 'es-AR', 'en-US' },
-            log_level = "error",
-            path = client.config.root_dir .. "/.ltex",
-        })
     end,
     capabilities = def_opts.capabilities,
-    filetypes = M.filetypes,
-    settings = { ltex = M.settings },
-    -- Look for existing `.ltex` directory first. If it doesn't exist,
-    -- look for .git/.hg directories. If everything else fails, get absolute
-    -- path to the file parent
-    root_dir = function(file_path)
-        return require("lspconfig").util.root_pattern('.ltex', '.hg', '.git')(file_path)
-            or vim.fn.fnamemodify(file_path, ':p:h')
-    end,
+    filetypes = { 'bib', 'markdown', 'org', 'plaintex', 'rst', 'rnoweb', 'tex' },
+    settings = {
+        ltex = {
+            checkFrequency = 'save',
+            language = { "en-US", 'es-AR' },
+            additionalRules = {
+                enablePickyRules = true,
+                motherTongue = 'es-AR',
+            },
+        },
+    },
 }
-
-return M.opts
--- lua/plugins/langs/ltex_extra.lua
+++ lua/plugins/langs/ltex_extra.lua
@@ -1,36 +1,43 @@
-local ltex_filetypes = { 'bib', 'markdown', 'org', 'plaintex', 'rst', 'rnoweb', 'tex' }
-
-local ltex_opts = {
-    settings = {
-        checkFrequency = 'save',
-        language = { "en-US", 'es-AR' },
-        additionalRules = {
-            enablePickyRules = true,
-            motherTongue = 'es-AR',
-        },
-    }
-}
-
 return {
     "barreiroleo/ltex_extra.nvim",
-    ft = { "markdown", "tex" },
-    opts = {
-        init_check = true,                 -- boolean : whether to load dictionaries on startup
-        load_langs = { 'es-AR', 'en-US' }, -- table <string> : language for witch dictionaries will be loaded
-        log_level = "info",                -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
-        path = ".ltex",                    -- string : path to store dictionaries. Relative path uses current working directory
-        -- path = vim.fn.expand("~") .. "/.local/share/ltex",
-        server_start = false,              -- boolean : Enable the call to ltex. Usefull for migration and test
-        -- server_opts = {
-        --     on_attach = function(client, bufnr)
-        --         print("Loading ltex from ltex_extra")
-        --         require("core.lsp.opts").on_attach(client, bufnr)
-        --     end,
-        --     capabilities = require("core.lsp.opts").capabilities,
-        --     filetypes = ltex_filetypes,
-        --     settings = {
-        --         ltex = ltex_opts
-        --     }
-        -- },
-    },
+    -- opts = {
+    --     init_check = true,   -- boolean : whether to load dictionaries on startup
+    --     path = ".ltex",      -- string : path to store dictionaries. Relative path uses current working directory
+    --     -- path = vim.fn.expand("~") .. "/.local/share/ltex",
+    --     server_start = true, -- boolean : Enable the call to ltex. Usefull for migration and test
+    --     server_opts = {
+    --         on_attach = function(client, bufnr)
+    --             print("Loading ltex from ltex_extra")
+    --             require("core.lsp.opts").on_attach(client, bufnr)
+    --         end,
+    --         capabilities = require("core.lsp.opts").capabilities,
+    --         filetypes = { 'bib', 'markdown', 'org', 'plaintex', 'rst', 'rnoweb', 'tex' },
+    --         settings = {
+    --             ltex = {
+    --                 settings = {
+    --                     checkFrequency = 'save',
+    --                     language = { "en-US", 'es-AR' },
+    --                     additionalRules = {
+    --                         enablePickyRules = true,
+    --                         motherTongue = 'es-AR',
+    --                     },
+    --                 }
+    --             },
+    --         }
+    --     },
+    -- },
+    config = function()
+        local function find_root()
+            local file_path = vim.api.nvim_buf_get_name(0)
+            local root_pattern = require("lspconfig").util.root_pattern
+            -- Look for existing `.ltex` directory first. If it doesn't exist,
+            -- look for .git/.hg directories. If everything else fails, get absolute path to the file parent
+            return root_pattern('.ltex', '.hg', '.git')(file_path) or vim.fn.fnamemodify(file_path, ':p:h')
+        end
+        require("ltex_extra").setup({
+            load_langs = { 'es-AR', 'en-US' }, -- table <string> : language for witch dictionaries will be loaded
+            log_level = "info",                -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
+            path = find_root(),                -- string : path to store dictionaries. Relative path uses current working directory
+        })
+    end
 }
