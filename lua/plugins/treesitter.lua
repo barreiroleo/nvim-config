480d65648ed4eea8d59b133c328646a24bdc5fa6
Leo Barreiro <a.leo.barreiro@gmail.com>
2026-04-28 22:23:35 -0300

    fix!: migrate tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,58 +1,93 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter/discussions/8621#discussioncomment-16411732
+
+local default_langs = { "c", "cpp", "make", "cmake",
+    "lua", "luadoc", "vim", "vimdoc",
+    "rust", "java", "python", "bash",
+    "markdown",
+    "json", "toml", "yaml", "sql", "dockerfile",
+}
+
+local function is_parser_installed(lang)
+    local installed = require("nvim-treesitter").get_installed()
+    return vim.tbl_contains(installed, lang)
+end
+
+local function is_parser_available(lang)
+    local available = require("nvim-treesitter").get_available()
+    return vim.tbl_contains(available, lang)
+end
+
+local function start_treesitter(buf, lang)
+    if not vim.treesitter.language.add(lang) then
+        vim.notify(
+            "Cannot load treesitter parser for language " .. lang,
+            vim.log.levels.WARN
+        )
+        return
+    end
+    vim.treesitter.start(buf)
+    vim.bo[buf].syntax = "ON"
+    if vim.treesitter.query.get(lang, "indents") then
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
+end
+
+---@param ev vim.api.keyset.create_autocmd.callback_args
+local function filetype_hook(ev)
+    local lang = vim.treesitter.language.get_lang(ev.match)
+    if not lang then
+        return
+    end
+    local buf = ev.buf
+    if is_parser_installed(lang) then
+        start_treesitter(buf, lang)
+    elseif is_parser_available(lang) then
+        require("nvim-treesitter").install({ lang }):await(function()
+            start_treesitter(buf, lang)
+        end)
+    end
+end
+
 return {
-    'nvim-treesitter/nvim-treesitter',
-    event = { 'BufNewFile', 'BufReadPre' },
-    build = ':TSUpdate',
-    main = "nvim-treesitter.configs", -- Sets main module to use for opts
-    dependencies = {
-        { "nvim-treesitter/nvim-treesitter-textobjects" },
-        {
-            'nvim-treesitter/nvim-treesitter-context',
-            opts = { multiline_threshold = 1, } -- Avoid showing '{' only lines
-        }
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        build = ':TSUpdate',
+        branch = "main",
+        init = function()
+            require('nvim-treesitter').install(default_langs):wait(300000) -- wait max. 5 min
+            vim.api.nvim_create_autocmd("FileType", { callback = filetype_hook, })
+        end
     },
-    opts = {
-        ensure_installed = {
-            "c", "cpp", "make", "cmake",
-            "lua", "luadoc", "vim", "vimdoc",
-            "rust", "java", "python", "bash",
-            "markdown",
-            "json", "toml", "yaml", "sql", "dockerfile",
-        },
-        ignore_install = { "javascript" },
-        auto_install = true,
-        sync_install = false,
-        highlight = {
-            enable = true,
-            --- Disable TS highlighting for certain conditions, like buffer size or language.
-            ---@param lang string
-            ---@param _ integer: bufnr
-            disable = function(lang, _)
-                if lang == "latex" or lang == "cpp" then
-                    return true
-                end
-            end,
-            additional_vim_regex_highlighting = { 'latex' },
-        },
-        indent = { enable = true }, -- Indentation based on treesitter for the = operator.
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                    ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-                },
-            },
-            move = {
-                enable = true,
-                goto_next_start = { ["]f"] = "@function.outer" },     -- ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-                goto_next_end = { ["]F"] = "@function.outer" },       -- ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-                goto_previous_start = { ["[f"] = "@function.outer" }, -- ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-                goto_previous_end = { ["[F"] = "@function.outer" },   -- ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        dependencies = 'nvim-treesitter/nvim-treesitter',
+        opts = { multiline_threshold = 1, } -- Avoid showing '{' only lines
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = 'nvim-treesitter/nvim-treesitter',
+        opts = {
+            textobjects = {
+                select = { lookahead = true, },
+                move = { set_jumps = true, },
             },
         },
+        init = function()
+            local select = require "nvim-treesitter-textobjects.select".select_textobject
+            vim.keymap.set({ "x", "o" }, "af", function() select("@function.outer", "textobjects") end)
+            vim.keymap.set({ "x", "o" }, "if", function() select("@function.inner", "textobjects") end)
+            vim.keymap.set({ "x", "o" }, "ac", function() select("@class.outer", "textobjects") end)
+            vim.keymap.set({ "x", "o" }, "ic", function() select("@class.inner", "textobjects") end, { desc = "Select inner part of a class region" })
+            vim.keymap.set({ "x", "o" }, "as", function() select("@local.scope", "locals") end, { desc = "Select language scope" })
+
+            local move = require("nvim-treesitter-textobjects.move")
+            vim.keymap.set({ "n", "x", "o" }, "]f", function() move.goto_next_start("@function.outer", "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "]F", function() move.goto_next_end("@function.outer", "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "[f", function() move.goto_previous_start("@function.outer", "textobjects") end)
+            vim.keymap.set({ "n", "x", "o" }, "[F", function() move.goto_previous_end("@function.outer", "textobjects") end)
+        end
     }
 }
